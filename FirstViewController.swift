72d33180a3f9d81b27090791f0580dfd9a3c97a7
sterilingmason <sterilinghb@gmail.com>
2017-05-17 02:59:38 -0400

    Initial commit

---
+++ FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  HealthSnap
+//
+//  Created by Apple on 5/17/17.
+//  Copyright © 2017 Apple. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
